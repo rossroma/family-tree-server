1cc0ee1694bd2c4b07cc643e29eb6f010b68b067
杨凯 <906580223@qq.com>
2023-02-13 19:56:46 +0800

    fix: change route path

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ server.use('/', router(express.Router()))
 server.use(express.static('./dist'))
 
 server.listen(8000, () => {
-  console.log('http://127.0.0.1:80 启动')
+  console.log('http://127.0.0.1:8000 启动')
 })
--- server/router.js
+++ server/router.js
@@ -3,16 +3,16 @@ const User = require('./controller/user')
 const { init } = require('./service/input')
 
 module.exports = router => {
-  router.post('/list/get', Manager.getList)
-  router.post('/list/getTree', Manager.getTreeList)
-  router.post('/item/create', Manager.createItem)
-  router.post('/item/get', Manager.getItem)
-  router.post('/item/remove', Manager.removeItem)
-  router.post('/item/update', Manager.updateItem)
-  router.post('/user/login', User.login)
-  router.get('/user/info', User.info)
-  router.post('/logout', User.logout)
-  router.post('/initData', init)
+  router.post('/api/list/get', Manager.getList)
+  router.post('/api/list/getTree', Manager.getTreeList)
+  router.post('/api/item/create', Manager.createItem)
+  router.post('/api/item/get', Manager.getItem)
+  router.post('/api/item/remove', Manager.removeItem)
+  router.post('/api/item/update', Manager.updateItem)
+  router.post('/api/user/login', User.login)
+  router.get('/api/user/info', User.info)
+  router.post('/api/logout', User.logout)
+  router.post('/api/initData', init)
 
   return router
 }
